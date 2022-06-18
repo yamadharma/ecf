6815f0389bb3ee1492b065de111973e414153790
Dmitry S. Kulyabov <yamadharma@gmail.com>
2022-06-18 16:53:40 +0300

    feat(org): add org-modern-indent preliminary

--- src/rc.d/rc.packages.el
+++ src/rc.d/rc.packages.el
@@ -281,6 +281,7 @@
 (desire 'org-tree-slide)
 
 (desire 'org-superstar)
+;; (desire 'org-modern-indent :recipe '(:fetcher github :repo "jdtsmith/org-modern-indent" :branch "main" :files ("*.el")))
 (desire 'org-modern)
 (desire 'org-super-agenda)
 
