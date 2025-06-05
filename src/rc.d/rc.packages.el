9b3aa3eb0ddcc2a546df755d6db8307853ca437d
Dmitry S. Kulyabov <yamadharma@gmail.com>
2025-06-05 15:43:59 +0300

    feat(packages): add shrface package

--- src/rc.d/rc.packages.el
+++ src/rc.d/rc.packages.el
@@ -180,6 +180,8 @@
 
 ;;;}}}
 
+(desire 'shrface)
+
 ;;;; Completion
 
 ;; (desire 'company)
@@ -351,7 +353,7 @@
 (desire 'org-custom-cookies)
 ;; (desire 'svg-tag-mode)
 (desire 'org-superstar)
-(desire 'org-modern-indent :recipe '(:fetcher github :repo "jdtsmith/org-modern-indent" :branch "main" :files ("*.el")))
+;; (desire 'org-modern-indent :recipe '(:fetcher github :repo "jdtsmith/org-modern-indent" :branch "main" :files ("*.el")))
 (desire 'org-modern)
 ;; (desire 'org-contacts)
 ;; (desire 'google-contacts)
